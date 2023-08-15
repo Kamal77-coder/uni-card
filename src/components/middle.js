0738e81aeddb3890343a4d7d92a571b64f522136
Kamal77-coder <kmlrohilla7065130@gmail.com>
2023-08-15 18:43:12 +0530

    change

--- src/components/middle.js
+++ src/components/middle.js
@@ -7,9 +7,9 @@ function Middle() {
   return (
     <div className="middle">
        <div><strong>Earn 1% assured cashback </strong>on your spends. Get 5X</div>
-       <div>more value than cashback at the Uni Store. Enjoy</div>
-       <div>round the clock Whatsapp support. And it's</div>
-       <div>lifetime free; no joining fee, no annual charges.</div>
+       <div><strong>more value than cashback</strong> at the Uni Store. Enjoy</div>
+       <div>round the clock <strong>Whatsapp support.</strong> And it's</div>
+       <div><strong>lifetime free</strong>; no joining fee, no annual charges.</div>
     </div>
   );
 }
